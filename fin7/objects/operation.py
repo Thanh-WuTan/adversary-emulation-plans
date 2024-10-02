0ad64e32ac5d51e2f7c9bce8d9670912178f92d6
root <wt.vathanh@gmail.com>
2024-10-02 13:32:29 +0000

    m

---
+++ fin7/objects/operation.py
@@ -0,0 +1,71 @@
+import time
+
+from main import PAYLOAD_DIR
+from objects.base_planning import BasePlanningService
+from objects.executor import Executor
+from objects.link import Link
+from objects.attire_logging import Attire
+
+
+class Operation:
+    def __init__(self, adversary=None, agents=None, source=None, learner=None, rules=[]):
+        self.adversary = adversary
+        self.agents = agents if agents else []
+        self.source = source
+        self.learner = learner  
+        self.rules = rules
+        if source:
+            self.rules = source.rules
+    
+    def active_agents(self):
+        return self.agents
+
+    def all_facts(self):
+        return self.source.facts
+
+    def all_relationships(self):
+        return self.source.relationships
+
+    def run(self):
+        attire = Attire(agent=self.agents[0])
+        bps = BasePlanningService()
+        agent = self.agents[0]
+        source = self.source
+        learner = self.learner 
+        for order, ability in enumerate(self.adversary.abilities, start=1):
+            print("=" * 60)
+            print("Running the %d/%d ability %s" % (order, len(self.adversary.abilities), ability['name']))
+            if not agent.is_capable_to_run(ability):
+                print("Abilitty is not capable to run")
+                print("=" * 60)
+                continue
+            executors = agent.find_executors(ability)
+            links = []  
+            for ex in executors:
+                executor = Executor(name = ex['name'], platform = ex['platform'], command = ex['command'],
+                                    parsers = ex['parsers'], timeout = ex['timeout'], payloads = ex['payloads'])
+                ex_link = Link(command=ex['command'], ability=ability, executor=executor, paw=agent.paw)
+                valid_links = bps.add_test_variants(links=[ex_link], agent=agent, facts=source.facts, rules=self.rules,
+                                                    trim_unset_variables=True, trim_missing_requirements=True,
+                                                    operation=self)
+                if valid_links:
+                    links = bps.sort_links(valid_links)
+                    break
+            ran_command = set()
+            steps = []
+            for step_order, link in enumerate(links, start=1):
+                ex = link.executor
+                ex.command = link.command
+                if ex.command in ran_command:
+                    continue
+                ran_command.add(ex.command)
+                print("-" * 60)
+                print("Executing the %d-th link" % (step_order))
+                print("\nCommand: %s" % link.command)
+                result = ex.run_command()
+                steps.append(learner._save(link=link, result=result, operation=self, executor=ex, step_order=step_order))
+                print("\nStdout: %s" % result.stdout)
+                print("\nStderr: %s" % result.stderr)
+                time.sleep(2)
+            attire.add_procedure(steps, ability, order) 
+        attire.create_attire_file()
